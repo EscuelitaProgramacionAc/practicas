74c68edba153bd972cee341891dc03958169d356
Leandro Bruzzi <lbruzzi@santandertecnologia.com>
2020-07-19 02:14:53 -0300

    Modificaciones

--- clase 5/js/clase5.js
+++ clase 5/js/clase5.js
@@ -1,5 +1,6 @@
+/*
 var Enviar = alert('Usted se pre-inscribió correctamente. A continuación recibirá un correo detallando los siguientes pasos');
 if (Enviar==true){
      console.log('Usted se pre-inscribió correctamente. A continuación recibirá un correo detallando los siguientes pasos');   
-
 }
+*/
--- clase 5/js/footercomponent.js
+++ clase 5/js/footercomponent.js
@@ -1,20 +1,18 @@
 var footerHTML = `
-<div class="row">
-        <div id="pie" class="col-12 verde row">
-          <div class="col-md-10">
-            <span> Tel: +54 11 6589 4598 </span>
-            <br/>
-            <span> Email:MICE.Events@gmail.com </span>
-            <br/>
-            <span> Dirección: El Salvador 4498, CABA </span>
-          </div>
+<div class="row m-separador align-items-center" id="pie">
+    <div class="col-12 col-md-6 texto-center">
+      <span> Tel: +54 11 6589 4598 </span>
+      <br/>
+      <span> Email:MICE.Events@gmail.com </span>
+      <br/>
+      <span> Dirección: El Salvador 4498, CABA </span>
+    </div>
 
-        <div class="col-md-2 justify-content-end">
-            <img src="./img/face.png" alt="10px" width="20px" />
-            <img src="./img/whatsapplogo.png" alt="10px" width="20px" />
-            <img src="./img/instalogo.png" alt="10px" width="20px" />
-          </div>
-        </div>
-      </div>`;
+    <div class="col-12 col-md-6 texto-center" style="text-align: right;">
+      <img src="./img/face.png" alt="10px" width="30px" />
+      <img src="./img/whatsapplogo.png" alt="10px" width="30px" />
+      <img src="./img/instalogo.png" alt="10px" width="30px" />
+    </div>
+</div>`;
 
-      $("#footer-js").append(footerHTML);
+$("#footer-js").append(footerHTML);
