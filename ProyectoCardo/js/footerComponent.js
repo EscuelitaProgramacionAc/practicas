c35e095af4272b4a2cb3504d19d2b58cd0ba6aff
Matias Cardo <cardomatias@gmail.com>
2020-07-21 19:03:14 -0300

    Cambios

--- ProyectoCardo/js/footerComponent.js
+++ ProyectoCardo/js/footerComponent.js
@@ -1,4 +1,9 @@
 var footerHTML = ` 
-
+<footer class="footer mt-auto py-3">
+<div class="container">
+    <img src="./img/instagram.png" class="img-fluid" alt="Responsive image"  width="4%"> 
+  <div class="text text-right"> Matias Cardo © 2020 </div>
+</div>
+</footer>
 `;
 $("#footer-js").append(footerHTML);
